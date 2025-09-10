f6d5a770dc2cc8a9e65f1708ec281898dddaddbf
Bryan <mrbhron@gmail.com>
2025-09-10 15:40:22 -0300

    Update script.js
    Arrumando o video atomic

--- js/script.js
+++ js/script.js
@@ -21,14 +21,14 @@ tl.to(".pedra", { x: 0, y: -100, filter: "blur(20px)", duration: 1 })
     .to(".parallax-h1", { x: -400, y: 1000, fontSize: "100px", duration: 1 }, "-=1")
     .to(".text-01-left", {x: -500, ease: "linear",}, "-=1")
     .to(".text-02-right", {x: 500, ease: "linear",}, "-=1")
-    .to(".video-final", {x: 0, y: 5000,},"-=1");
+    .to(".video-final", {x: 0, y: 4500,},"-=1");
 
 // === BLOCO 2: cada elemento sozinho ===
 tl.to(".bota-frente", { y: "+=800", rotation: "+=30", duration: 1.5, width: 1000, overwrite: "auto" })
     .to(".h2-text-bt-frente", { opacity: 1, y: "+=1300", x: -380, duration: 1.5 }, "-=1.5")
     .to(".bota-tras", { x: "-=400", y: "+=2200", rotation: "-=35", duration: 1.5, width: 1100, overwrite: "auto" })
     .to(".h2-text-bt-tras", { opacity: 1, y: "+=2700", x: 250, duration: 1.5 }, "-=1.5")
-    .to(".video-final", {x: 0, y: "-=1620", duration: 1, ease: "linear", zIndex: 100,},);
+    .to(".video-final", {x: 0, y: "-=1100", duration: 1, ease: "linear", zIndex: 100,},);
     
 
 
@@ -43,4 +43,5 @@ buttons.forEach(btn => {
     buttons.forEach(b => b.classList.remove('active'));
     btn.classList.add('active');
   });
+
 });
